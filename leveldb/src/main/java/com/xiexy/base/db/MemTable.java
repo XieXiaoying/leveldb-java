9d277a751d5f594d4601ae39c84fac62286bbedb
xie <xxiaoying321@163.com>
2020-11-23 23:41:09 +0800

    add commit

--- leveldb/src/main/java/com/xiexy/base/db/MemTable.java
+++ leveldb/src/main/java/com/xiexy/base/db/MemTable.java
@@ -25,8 +25,19 @@ import java.util.concurrent.atomic.AtomicLong;
 import static com.xiexy.base.utils.DataUnit.LONG_UNIT;
 import static java.util.Objects.requireNonNull;
 
+
 /**
- * ConcurrentSkipListMap是MemTable的核心数据结构，memtable的KV数据都存储在ConcurrentSkipListMap中
+ * leveldb之所以有level这个单词就是因为数据存储分层管理，而日志和内存表（memtable）处于第0层
+ * 在Leveldb中，所有内存中的KV数据都存储在Memtable中，物理disk则存储在SSTable中。
+ * 在系统运行过程中，如果Memtable中的数据占用内存到达指定值(Options.write_buffer_size)，
+ * 则Leveldb就自动将Memtable转换为Immutable Memtable，并自动生成新的Memtable，也就是Copy-On-Write机制了。
+ * Immutable Memtable则被新的线程Dump到磁盘中，Dump结束则该Immutable Memtable就可以释放了。因名知意，Immutable Memtable是只读的。
+ *
+ * 所以可见，最新的数据都是存储在Memtable中的，Immutable Memtable和物理SSTable则是某个时点的数据。
+ * Memtable提供了写入KV记录，删除以及读取KV记录的接口，
+ * 但是事实上Memtable并不执行真正的删除操作,删除某个Key的Value在Memtable内是作为插入一条记录实施的，但是会打上一个Key的删除标记，
+ * 真正的删除操作在后面的 Compaction过程中，lazy delete。
+ * ConcurrentSkipListMap是MemTable的核心数据结构，memtable的KV数据都存储在ConcurrentSkipListMap中。
  */
 public class MemTable
         implements SeekingIterable<InternalKey, Slice>
